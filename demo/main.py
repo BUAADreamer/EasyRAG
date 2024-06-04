a746546f8df15809463191d6aed03b6d9ce6ce89
BUAADreamer <1428195643@qq.com>
2024-06-04 20:02:57 +0800

    init

---
+++ demo/main.py
@@ -0,0 +1,74 @@
+import asyncio
+
+from dotenv import dotenv_values
+from llama_index.core import Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.legacy.llms import OpenAILike as OpenAI
+from qdrant_client import models
+from tqdm.asyncio import tqdm
+
+from pipeline.ingestion import build_pipeline, build_vector_store, read_data
+from pipeline.qa import read_jsonl, save_answers
+from pipeline.rag import QdrantRetriever, generation_with_knowledge_retrieval
+
+
+async def main():
+    config = dotenv_values(".env")
+
+    # 初始化 LLM 嵌入模型 和 Reranker
+    llm = OpenAI(
+        api_key=config["GLM_KEY"],
+        model="glm-4",
+        api_base="https://open.bigmodel.cn/api/paas/v4/",
+        is_chat_model=True,
+    )
+    embeding = HuggingFaceEmbedding(
+        model_name="BAAI/bge-small-zh-v1.5",
+        cache_folder=config.get("hfmodel_cache_folder"),
+        embed_batch_size=128,
+    )
+    Settings.embed_model = embeding
+
+    # 初始化 数据ingestion pipeline 和 vector store
+    client, vector_store = await build_vector_store(config, reindex=False)
+
+    collection_info = await client.get_collection(
+        config["COLLECTION_NAME"] or "aiops24"
+    )
+    data_path = config.get("data_path", "data")
+    if collection_info.points_count == 0:
+        data = read_data(data_path)
+        pipeline = build_pipeline(llm, embeding, vector_store=vector_store)
+        # 暂时停止实时索引
+        await client.update_collection(
+            collection_name=config["COLLECTION_NAME"] or "aiops24",
+            optimizer_config=models.OptimizersConfigDiff(indexing_threshold=0),
+        )
+        await pipeline.arun(documents=data, show_progress=True, num_workers=1)
+        # 恢复实时索引
+        await client.update_collection(
+            collection_name=config["COLLECTION_NAME"] or "aiops24",
+            optimizer_config=models.OptimizersConfigDiff(indexing_threshold=20000),
+        )
+        print(len(data))
+
+    retriever = QdrantRetriever(vector_store, embeding, similarity_top_k=3)
+
+    queries = read_jsonl("question.jsonl")
+
+    # 生成答案
+    print("Start generating answers...")
+
+    results = []
+    for query in tqdm(queries, total=len(queries)):
+        result = await generation_with_knowledge_retrieval(
+            query["query"], retriever, llm
+        )
+        results.append(result)
+
+    # 处理结果
+    save_answers(queries, results, "submit_result.jsonl")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
